0546467357e618b289110a52ec006d78a4756a79
Lovelyfin00 <38161296+Lovelyfin00@users.noreply.github.com>
2022-07-08 21:42:44 +0100

    Add files via upload

---
+++ js/dashboard.js
@@ -0,0 +1,447 @@
+// Calling the object for JULY from apexcharts
+
+var options6 ={
+    series: [{
+    name: 'Petrol',
+    data: [19, 0, 0]
+  }, {
+    name: 'Diesel',
+    data: [38, 0, 0]
+  }, {
+    name: 'Prepaid',
+    data: [0, 22, 0]
+  },
+  {
+    name: 'PostPaid',
+    data: [0, 30, 0]
+  },
+  {
+    name: 'solar',
+    data: [0, 0, 9]
+  },
+  {
+    name: 'Wind',
+    data: [0, 0, 22]
+  }
+],
+    chart: {
+    type: 'bar',
+    height: 350
+  },
+  plotOptions: {
+    bar: {
+      horizontal: false,
+      columnWidth: '100%',
+      endingShape: 'rounded'
+    },
+  },
+  dataLabels: {
+    enabled: false
+  },
+  stroke: {
+    show: true,
+    width: 0,
+    colors: ['solid']
+  },
+  xaxis: {
+    categories: ['Generator', 'Utility', 'Renewable'],
+  },
+  colors: [
+    '#ED1C24', '#5B40C5', '#17D74D', '#3567DB', '#62C7FF', '#FBB500'
+],
+  fill: {
+    opacity: 1
+  },
+  tooltip: {
+    y: {
+      formatter: function (val) {
+        return + val + "%"
+      }
+    }
+  }
+  };
+
+ 
+  // Calling the object for JUNE from apexcharts
+
+var options5 ={
+  series: [{
+  name: 'Petrol',
+  data: [40, 0, 0]
+}, {
+  name: 'Diesel',
+  data: [33, 0, 0]
+}, {
+  name: 'Prepaid',
+  data: [0, 15, 0]
+},
+{
+  name: 'PostPaid',
+  data: [0, 75, 0]
+},
+{
+  name: 'solar',
+  data: [0, 0, 45]
+},
+{
+  name: 'Wind',
+  data: [0, 0, 5]
+}
+],
+  chart: {
+  type: 'bar',
+  height: 350
+},
+plotOptions: {
+  bar: {
+    horizontal: false,
+    columnWidth: '100%',
+    endingShape: 'rounded'
+  },
+},
+dataLabels: {
+  enabled: false
+},
+stroke: {
+  show: true,
+  width: 0,
+  colors: ['solid']
+},
+xaxis: {
+  categories: ['Generator', 'Utility', 'Renewable'],
+},
+fill: {
+  opacity: 1
+},
+tooltip: {
+  y: {
+    formatter: function (val) {
+      return + val + "%"
+    }
+  }
+}
+};
+var options4 ={
+  series: [{
+  name: 'Petrol',
+  data: [30, 0, 0]
+}, {
+  name: 'Diesel',
+  data: [30, 0, 0]
+}, {
+  name: 'Prepaid',
+  data: [0, 15, 0]
+},
+{
+  name: 'PostPaid',
+  data: [0, 75, 0]
+},
+{
+  name: 'solar',
+  data: [0, 0, 45]
+},
+{
+  name: 'Wind',
+  data: [0, 0, 5]
+}
+],
+  chart: {
+  type: 'bar',
+  height: 350
+},
+plotOptions: {
+  bar: {
+    horizontal: false,
+    columnWidth: '100%',
+    endingShape: 'rounded'
+  },
+},
+dataLabels: {
+  enabled: false
+},
+stroke: {
+  show: true,
+  width: 0,
+  colors: ['solid']
+},
+xaxis: {
+  categories: ['Generator', 'Utility', 'Renewable'],
+},
+fill: {
+  opacity: 1
+},
+tooltip: {
+  y: {
+    formatter: function (val) {
+      return + val + "%"
+    }
+  }
+}
+};
+
+var options3 ={
+  series: [{
+  name: 'Petrol',
+  data: [40, 0, 0]
+}, {
+  name: 'Diesel',
+  data: [33, 0, 0]
+}, {
+  name: 'Prepaid',
+  data: [0, 15, 0]
+},
+{
+  name: 'PostPaid',
+  data: [0, 75, 0]
+},
+{
+  name: 'solar',
+  data: [0, 0, 45]
+},
+{
+  name: 'Wind',
+  data: [0, 0, 5]
+}
+],
+  chart: {
+  type: 'bar',
+  height: 350
+},
+plotOptions: {
+  bar: {
+    horizontal: false,
+    columnWidth: '100%',
+    endingShape: 'rounded'
+  },
+},
+dataLabels: {
+  enabled: false
+},
+stroke: {
+  show: true,
+  width: 0,
+  colors: ['solid']
+},
+xaxis: {
+  categories: ['Generator', 'Utility', 'Renewable'],
+},
+fill: {
+  opacity: 1
+},
+tooltip: {
+  y: {
+    formatter: function (val) {
+      return + val + "%"
+    }
+  }
+}
+};
+var options2 ={
+  series: [{
+  name: 'Petrol',
+  data: [40, 0, 0]
+}, {
+  name: 'Diesel',
+  data: [33, 0, 0]
+}, {
+  name: 'Prepaid',
+  data: [0, 15, 0]
+},
+{
+  name: 'PostPaid',
+  data: [0, 75, 0]
+},
+{
+  name: 'solar',
+  data: [0, 0, 45]
+},
+{
+  name: 'Wind',
+  data: [0, 0, 5]
+}
+],
+  chart: {
+  type: 'bar',
+  height: 350
+},
+plotOptions: {
+  bar: {
+    horizontal: false,
+    columnWidth: '100%',
+    endingShape: 'rounded'
+  },
+},
+dataLabels: {
+  enabled: false
+},
+stroke: {
+  show: true,
+  width: 0,
+  colors: ['solid']
+},
+xaxis: {
+  categories: ['Generator', 'Utility', 'Renewable'],
+},
+colors:["#FFC54D", "#FF7396", "#53BF9D", "#000000", "#F94C66", "#54BAB9"],
+fill: {
+  opacity: 1
+},
+tooltip: {
+  y: {
+    formatter: function (val) {
+      return + val + "%"
+    }
+  }
+}
+};
+var options1 ={
+  series: [{
+  name: 'Petrol',
+  data: [40, 0, 0]
+}, {
+  name: 'Diesel',
+  data: [33, 0, 0]
+}, {
+  name: 'Prepaid',
+  data: [0, 15, 0]
+},
+{
+  name: 'PostPaid',
+  data: [0, 75, 0]
+},
+{
+  name: 'solar',
+  data: [0, 0, 45]
+},
+{
+  name: 'Wind',
+  data: [0, 0, 5]
+}
+],
+  chart: {
+  type: 'bar',
+  height: 350
+},
+plotOptions: {
+  bar: {
+    horizontal: false,
+    columnWidth: '100%',
+    endingShape: 'rounded'
+  },
+},
+dataLabels: {
+  enabled: false
+},
+stroke: {
+  show: true,
+  width: 0,
+  colors: ['solid']
+},
+xaxis: {
+  categories: ['Generator', 'Utility', 'Renewable'],
+},
+fill: {
+  opacity: 1
+},
+tooltip: {
+  y: {
+    formatter: function (val) {
+      return + val + "%"
+    }
+  }
+}
+};
+var options ={
+  series: [{
+  name: 'Petrol',
+  data: [40, 0, 0]
+}, {
+  name: 'Diesel',
+  data: [33, 0, 0]
+}, {
+  name: 'Prepaid',
+  data: [0, 15, 0]
+},
+{
+  name: 'PostPaid',
+  data: [0, 75, 0]
+},
+{
+  name: 'solar',
+  data: [0, 0, 45]
+},
+{
+  name: 'Wind',
+  data: [0, 0, 5]
+}
+],
+  chart: {
+  type: 'bar',
+  height: 350
+},
+plotOptions: {
+  bar: {
+    horizontal: false,
+    columnWidth: '100%',
+    endingShape: 'rounded'
+  },
+},
+dataLabels: {
+  enabled: false
+},
+stroke: {
+  show: true,
+  width: 0,
+  colors: ['solid']
+},
+xaxis: {
+  categories: ['Generator', 'Utility', 'Renewable'],
+},
+fill: {
+  opacity: 1
+},
+tooltip: {
+  y: {
+    formatter: function (val) {
+      return + val + "%"
+    }
+  }
+}
+};
+//   Display for July without selection
+
+var charts = new ApexCharts(document.querySelector("#efficiency-chart"), options6);
+charts.render();
+
+// selecting the DOM for the select
+
+let selectEfficiency = document.querySelector("#efficiency-select");
+
+// If and else
+
+selectEfficiency.addEventListener("change", ()=>{
+    let efficiencyIndex = selectEfficiency.selectedIndex;
+    if (efficiencyIndex === 6){
+      var charts = new ApexCharts(document.querySelector("#efficiency-chart"), options5);
+      charts.render();
+    } else if (efficiencyIndex === 5){
+        var chart5 = new ApexCharts(document.querySelector("#efficiency-chart"), options5);
+        chart5.render();
+    }else if (efficiencyIndex === 4){
+        var chart = new ApexCharts(document.querySelector("#efficiency-chart"), options4);
+        chart.render();
+    }else if (efficiencyIndex === 3){
+        var chart = new ApexCharts(document.querySelector("#efficiency-chart"), options3);
+        chart.render();
+    }else if (efficiencyIndex === 2){
+        var chart = new ApexCharts(document.querySelector("#efficiency-chart"), options2);
+        chart.render();
+    }else if (efficiencyIndex === 1){
+        var chart = new ApexCharts(document.querySelector("#efficiency-chart"), options1);
+        chart.render();
+    } else {
+        var chart = new ApexCharts(document.querySelector("#efficiency-chart"), options);
+        chart.render();
+        console.log(efficiencyIndex)
+    }
+})
